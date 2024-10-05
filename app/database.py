1b82259cb6764f2e0651a6797b5cadf74239a0c6
m0wer <github@sgn.space>
2024-10-05 15:06:30 +0200

    fix: fix database url for api

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ def get_engine(db_url: str = DATABASE_URL):
     return create_engine(db_url, connect_args={"check_same_thread": False})
 
 
-def get_session(db_url: str = DATABASE_URL):
-    engine = get_engine(db_url)
+def get_session():
+    engine = get_engine(DATABASE_URL)
     with Session(engine) as session:
         yield session
